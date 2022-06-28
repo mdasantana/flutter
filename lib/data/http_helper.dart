d154a466af087b7a75b8b4c9f114297e53ba8198
Matheus Santana <matheusanjos_santana@hotmail.com>
2022-06-28 19:51:07 -0300

    Refatoracao da funcao de busca na api e implementacao das funcoes de consumo dentro da propria httpHelper

--- lib/data/http_helper.dart
+++ lib/data/http_helper.dart
@@ -1,26 +1,35 @@
-import 'package:flutter/cupertino.dart';
+import 'dart:math';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:pokeapp/data/pokemon.dart';
 
 class HttpHelper {
-  Future<Pokemon> getPokemonById(int pokemonId) async {
+  Future<Pokemon> _getPokemon({int? id, String? name}) async {
+    dynamic pokemonId = id ?? name;
     Uri uri = Uri.parse("https://pokeapi.glitch.me/v1/pokemon/$pokemonId");
     var result = await http.get(uri);
     List<dynamic> pokemonList = jsonDecode(result.body);
     Map<String, dynamic> pokemonData = pokemonList.first;
-    debugPrint(pokemonData.toString());
     Pokemon pokemon = Pokemon.fromJson(pokemonData);
     return pokemon;
   }
 
-  Future<Pokemon> getPokemonByName(String pokemonName) async {
-    Uri uri = Uri.parse("https://pokeapi.glitch.me/v1/pokemon/$pokemonName");
-    var result = await http.get(uri);
-    List<dynamic> pokemonList = jsonDecode(result.body);
-    Map<String, dynamic> pokemonData = pokemonList.first;
-    debugPrint(pokemonData.toString());
-    Pokemon pokemon = Pokemon.fromJson(pokemonData);
-    return pokemon;
+  // Future<Pokemon> _getPokemonByName(String pokemonName) async {
+  //   Uri uri = Uri.parse("https://pokeapi.glitch.me/v1/pokemon/$pokemonName");
+  //   var result = await http.get(uri);
+  //   List<dynamic> pokemonList = jsonDecode(result.body);
+  //   Map<String, dynamic> pokemonData = pokemonList.first;
+  //   Pokemon pokemon = Pokemon.fromJson(pokemonData);
+  //   return pokemon;
+  // }
+
+  Future<Pokemon> getPokemonById({int? id}) async {
+    Random random = Random();
+    int pokemonId = id ?? random.nextInt(807);
+    return await _getPokemon(id: pokemonId);
+  }
+
+  Future<Pokemon> getPokemonByName(String name) async {
+    return await _getPokemon(name: name);
   }
 }
