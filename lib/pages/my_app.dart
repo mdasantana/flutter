6fdcfddf2ae8812448a9410435d5e3bf18e4645e
Matheus Santana <matheusanjos_santana@hotmail.com>
2022-06-29 11:23:09 -0300

    Ajuste para retornar a pag Pokemon como a home

--- lib/pages/my_app.dart
+++ lib/pages/my_app.dart
@@ -1,22 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:pokeapp/pages/pokemons/pokemon/pokemon.page.dart';
-import 'package:pokeapp/pages/pokemons/pokemon/pokemons.page.dart';
-import 'my_home_page.dart';
+import 'package:pokeapp/pages/pokemon.page.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'PokeApp',
-      initialRoute: "home",
+      home: PokemonPage(),
       debugShowCheckedModeBanner: false,
-      routes: {
-        "home": (context) => const MyHomePage(),
-        "allpokemons": (context) => const PokemonsPage(),
-        // "pokemon": (context) => PokemonPage()
-      },
     );
   }
 }
