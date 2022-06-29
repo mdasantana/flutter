04a5c7d5fef19e473456677d67f418eddb704eb1
Matheus Santana <matheusanjos_santana@hotmail.com>
2022-06-29 11:19:22 -0300

    Criacao das constantes de texto do app

---
+++ lib/shared/constants/app_strings.dart
@@ -0,0 +1,12 @@
+class AppStrings{
+  static String get loadingText => "Buscando Pokémon...";
+  static String get detailDescriptionText => "DESCRIPTION";
+  static String get detailSizesText => "SIZES";
+  static String get detailHeightText => "HEIGHT";
+  static String get detailWeightText => "WEIGHT";
+  static String get detailLbsText => "lbs.";
+  static String get detailAbilitiesText => "ABILITIES";
+    static String get detailNormalText => "NORMAL";
+  static String get detailHiddenText => "HIDDEN";
+  static String get detailEvolutionLineText => "EVOLUTION LINE";
+}
